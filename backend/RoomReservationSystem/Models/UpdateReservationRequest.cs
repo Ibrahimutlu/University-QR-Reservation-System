7ff8a9230643ddc5bfc84456f88acee638238c93
rouby21 <robamassoud2004@gmail.com>
2026-04-30 22:20:55 +0300

    Added UpdateReservationRequest model

---
+++ backend/RoomReservationSystem/Models/UpdateReservationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RoomReservationSystem.Models
+{
+    public class UpdateReservationRequest
+    {
+        public DateTime ReservationDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
