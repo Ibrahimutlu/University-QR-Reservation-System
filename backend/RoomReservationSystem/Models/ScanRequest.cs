2ae5c825f20bec29f6ee665be8365614e58028a7
rouby21 <robamassoud2004@gmail.com>
2026-05-08 13:43:12 +0300

    Add ScanRequest model

---
+++ backend/RoomReservationSystem/Models/ScanRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoomReservationSystem.Models
+{
+    public class ScanRequest
+    {
+        public int UserID { get; set; }
+        public int RoomID { get; set; }
+        public DateTime ScanTime { get; set; }
+        public string ScanType { get; set; } // CheckIn or CheckOut
+    }
+}
